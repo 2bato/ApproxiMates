cdc5211ed93e9bfdd7d8b090f3c91e0be0e8d8a3
cclsunn <carolsunmail@gmail.com>
2024-01-20 20:38:36 -0800

    added file upload to profilepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import PostMain from "./components/PostMain";
 import Header from "./components/Header";
 import PostBox from "./components/PostBox";
 import PopupInput from "./components/PopupInput";
+import ProfilePage from "./components/ProfilePage";
 
 function App() {
   return (
@@ -21,6 +22,8 @@ function App() {
       <PostBox />
 
       <PopupInput />
+
+      <ProfilePage />
 
       <div className="fixed bottom-0 left-0 w-full mb-3">
         <Footer />
--- frontend/src/components/ProfilePage.js
+++ frontend/src/components/ProfilePage.js
@@ -1,7 +1,21 @@
 import React, {useState} from 'react'
 
 function ProfilePage(props) {
+    const [editProfile, setEditProfile] = useState(false);
+
+    const [username, setUsername] = useState("");
+    const [file, setFile] = useState();
     
+
+    return (
+        <div>
+            <h2>
+                upload profile picture
+            </h2>
+            <input type = 'file' onChange={(e) => {setFile(URL.createObjectURL(e.target.files[0]));}}/>
+            <img src = {file}/>
+        </div>
+    );
 }
 
 export default ProfilePage;
