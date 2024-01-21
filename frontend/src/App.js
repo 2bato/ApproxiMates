c7a3809f38fbc8ad81c00e9972a34987f6a77f31
ericxiong <ericex134@gmail.com>
2024-01-20 16:54:40 -0800

    c

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,21 +2,20 @@ import "./App.css";
 import Footer from "./components/Footer";
 import PostMain from "./components/PostMain";
 import Header from "./components/Header";
+import PostBox from "./components/PostBox";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <PostBox />
-      <PostMain 
-      type="Service"
-      locationType="Exact Location"
-      title="Pipe Bursted"
-      description="My Name is Andrew Qi Ming Fan and my pipe burst in my basement. I am sad but I love men"/>
+      <PostMain
+        type="Service"
+        locationType="Exact Location"
+        title="Pipe Bursted"
+        description="My Name is Andrew Qi Ming Fan and my pipe burst in my basement. I am sad but I love men"
+      />
 
-        
-
-      
       <div className="fixed bottom-0 left-0 w-full mb-3">
         <Footer />
       </div>
