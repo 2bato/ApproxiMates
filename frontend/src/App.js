a131bef2edc656ed0ceef66186f0f921df0f221f
ericxiong <ericex134@gmail.com>
2024-01-20 18:16:29 -0800

    Filter buttons completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <PostBox />
       <PostMain
         type="Service"
         locationType="Exact Location"
--- frontend/src/components/PostBox.js
+++ frontend/src/components/PostBox.js
@@ -1,5 +1,20 @@
 function PostBox() {
-  return <div>hello</div>;
+  const b =
+    "bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded";
+  return (
+    <header className="header">
+      <div>
+        <ul className="px-2 overflow-x-auto mt-2 items-center justify-center flex space-x-3">
+          <button className={b}>Alerts</button>
+          <button className={b}>Marketplace</button>
+          <button className={b}>Services</button>
+          <button className={b}>Events</button>
+          <button className={b}>Misc.</button>
+          <button className={b}>Random</button>
+        </ul>
+      </div>
+    </header>
+  );
 }
 
 export default PostBox;
